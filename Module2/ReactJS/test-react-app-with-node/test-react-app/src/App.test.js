40d9e01ccb138318ac25bdebfe1db96ebdf01d89
chichao89 <70385670+chichao89@users.noreply.github.com>
2020-12-17 19:55:12 +0800

    "changes"

--- Module2/ReactJS/test-react-app-with-node/test-react-app/src/App.test.js
+++ Module2/ReactJS/test-react-app-with-node/test-react-app/src/App.test.js
@@ -6,3 +6,15 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe("Filter function", () => {
+  test("it should filter by a search term (link)", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+
+    const output = [{ id: 3, url: "https://www.link3.dev" }];
+  });
+});
